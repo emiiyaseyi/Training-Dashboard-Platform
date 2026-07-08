9fdad6b26892fb05b36d60cbcf103cd451411274
emiiyaseyi <momlilyseyi@gmail.com>
2026-07-08 11:30:59 +0100

    Fix ternary-as-statement ESLint warning in StaffHoursTable

--- src/components/ui/StaffHoursTable.tsx
+++ src/components/ui/StaffHoursTable.tsx
@@ -33,7 +33,8 @@ export function StaffHoursTable({ staffDetail, hoursThreshold = 40 }: Props) {
   const toggle = (id: string) =>
     setExpanded((prev) => {
       const next = new Set(prev)
-      next.has(id) ? next.delete(id) : next.add(id)
+      if (next.has(id)) next.delete(id)
+      else next.add(id)
       return next
     })
 
